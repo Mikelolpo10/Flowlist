c266ddd35be790698932b6046efd561e8af50aa2
Mikelolpo10 <xzvmike@gmail.com>
2026-01-21 18:55:57 +0700

    Pakai pseudo before dan after pemisah sidebar nav

--- Flowlist/src/pages/Dashboard/Dashboard.jsx
+++ Flowlist/src/pages/Dashboard/Dashboard.jsx
@@ -8,15 +8,17 @@ export default function Dashboard() {
 
       <div id="root-container">
         <Sidebar />
-    
-        <div id="greetings-container">
-          <h1>Hello, Username.</h1>
-          <p>
-            Today you have 5 new task to be completed, 
-            2 meetings to attend, 5 event to participate 
-            in, and you have receive 28 message from 5 different people
-          </p>
-        </div>
+
+        <main id="main-container">
+          <div id="greetings-container">
+            <h1>Hello, Username.</h1>
+            <p>
+              Today you have 5 new task to be completed, 
+              2 meetings to attend, 5 event to participate 
+              in, and you have receive 28 message from 5 different people
+            </p>
+          </div>
+        </main>
       </div>
     </>
   )
