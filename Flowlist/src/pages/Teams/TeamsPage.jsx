20a8e579053a7cc986365f64e7d6adc0161e60ac
Mikelolpo10 <xzvmike@gmail.com>
2026-02-18 18:03:22 +0700

    Tambahin box shadow di search bar dan benerin var box shadow serta tambah var biru primary

--- Flowlist/src/pages/Teams/TeamsPage.jsx
+++ Flowlist/src/pages/Teams/TeamsPage.jsx
@@ -1,7 +1,10 @@
 import { motion } from 'motion/react'
+import { useState } from 'react'
 import './TeamsPage.css'
 
 export default function TeamsPage({ pageVariant, userData }) {
+  const [ searchBarFocus, setSearchBarFocus ] = useState('')
+
   return (
     <>
       <motion.div className='root-container'
@@ -17,11 +20,15 @@ export default function TeamsPage({ pageVariant, userData }) {
             <small>Manage and collaborate across all your teams</small>
           </div>
           <div id='header-right'>
-            <div id='search-bar'>
+            <div className={`search-bar ${searchBarFocus}`}>
               <svg viewBox="0 0 24 24" fill="none" stroke="#686868" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.35-4.35"/></svg>
-              <input placeholder="Search teams..."/>
+              <input 
+                placeholder="Search teams..." 
+                onFocus={() => setSearchBarFocus('search-bar-focus')} 
+                onBlur={() => setSearchBarFocus('')}
+              />
             </div>
-            <button>+ New Team</button>
+            <button id='search-button'>+ New Team</button>
           </div>
         </header>
 
