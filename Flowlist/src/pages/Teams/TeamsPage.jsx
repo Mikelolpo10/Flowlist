aec9cd0edc37acd24ae63329a9758f5da9164883
Mikelolpo10 <xzvmike@gmail.com>
2026-02-19 16:48:46 +0700

    Team choice done

--- Flowlist/src/pages/Teams/TeamsPage.jsx
+++ Flowlist/src/pages/Teams/TeamsPage.jsx
@@ -1,9 +1,29 @@
 import { motion } from 'motion/react'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './TeamsPage.css'
 
 export default function TeamsPage({ pageVariant, userData }) {
-  const [ isSearchFocus, setIsSearchFocus ] = useState(false)
+  const [isSearchFocus, setIsSearchFocus] = useState(false)
+  const teamChoicesRef = useRef()
+  const teamChoices = [
+    {
+      text: 'All Teams',
+      isActive: true
+    }, {
+      text: 'My Teams',
+      isActive: false
+    }, {
+      text: 'Archived',
+      isActive: false
+    }
+  ]
+
+  function switchTeamChoice(choice) {
+    const items = teamChoicesRef.current.querySelectorAll('.team-choice')
+    items.forEach(el => el.classList.remove('team-choice-active'))
+    choice.classList.add('team-choice-active')
+    console.log()
+  }
 
   return (
     <>
@@ -23,20 +43,20 @@ export default function TeamsPage({ pageVariant, userData }) {
           </div>
           <div id='header-right'>
             <div className={`search-bar ${isSearchFocus ? 'search-bar-focus' : ''}`}>
-              <svg 
-                viewBox="0 0 24 24" 
-                fill="none" 
-                stroke="#686868" 
-                strokeWidth="2" 
-                strokeLinecap="round" 
+              <svg
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#686868"
+                strokeWidth="2"
+                strokeLinecap="round"
                 strokeLinejoin="round"
               >
-                <circle cx="11" cy="11" r="8"/>
-                <path d="m21 21-4.35-4.35"/>
+                <circle cx="11" cy="11" r="8" />
+                <path d="m21 21-4.35-4.35" />
               </svg>
-              <input 
-                placeholder="Search teams..." 
-                onFocus={() => setIsSearchFocus(true)} 
+              <input
+                placeholder="Search teams..."
+                onFocus={() => setIsSearchFocus(true)}
                 onBlur={() => setIsSearchFocus(false)}
               />
             </div>
@@ -45,9 +65,23 @@ export default function TeamsPage({ pageVariant, userData }) {
         </header>
 
         <main id='teams-container'>
-          <div id='team-choice-container'>
-            
+          <div id='team-choices-container' ref={teamChoicesRef}>
+            {teamChoices.map(({ text, isActive }) => (
+              <div 
+                key={text} 
+                className={isActive ? `team-choice team-choice-active` : `team-choice `} 
+                onClick={(e) => switchTeamChoice(e.currentTarget)}
+              >
+                {text}
+              </div>
+            ))}
           </div>
+
+          <section id='stats-section'>
+            <div className="stats-card">
+              
+            </div>
+          </section>
         </main>
 
       </motion.div>
